c2c154d045a9cbfc940303c6099cc408b9b7b682
Theofanis Tsiantas <th.tsiantas@icloud.com>
2024-06-21 14:58:46 +0200

    Addition of code comments

--- PyLucene/IndexFiles.py
+++ PyLucene/IndexFiles.py
@@ -1,27 +1,27 @@
-#!/usr/bin/env python
-
 INDEX_DIR = "IndexFiles.index"
 
 import sys, os, lucene, threading, time
 import subprocess
-
-# import pandas as pd
-
 from java.nio.file import Paths
 from org.apache.lucene.analysis.standard import StandardAnalyzer
 from org.apache.lucene.document import Document, Field, FieldType
 from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig, IndexOptions
 from org.apache.lucene.store import NIOFSDirectory
 
+
+# Function for installing a package during run time
 def install(package):
     print("inside install")
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-install('pandas')
 
-import pandas as pd;
+# Install pandas to manipulate the movies csv
+install('pandas')
+import pandas as pd
+
 
 def indexMovies(analyzer, movies: pd.core.frame.DataFrame):
+    # Configuration settings
     store = NIOFSDirectory(Paths.get(os.path.join(os. getcwd(), INDEX_DIR)))
     config = IndexWriterConfig(analyzer)
     config.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
@@ -32,6 +32,7 @@ def indexMovies(analyzer, movies: pd.core.frame.DataFrame):
     t1.setTokenized(True)
     t1.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
 
+    # Add movies to documents for indexing
     idx = 0
     indexed_movies = []
     print("Reading movies")
@@ -43,9 +44,10 @@ def indexMovies(analyzer, movies: pd.core.frame.DataFrame):
         indexed_movies.append(doc)
         idx += 1
 
-
+    # Info print
     print("Indexing "+str(idx)+" movies")
-    startIndexTime = time.time()
+    # Index documents (movies)
+    start_index_time = time.time()
     idx = 0
     for doc in indexed_movies:
         if idx % 1000 == 0: print(".", end='', flush=True)
@@ -54,21 +56,18 @@ def indexMovies(analyzer, movies: pd.core.frame.DataFrame):
             idx += 1
         except Exception as e:
             print("Failed in indexMovies:", e)
-
-    elapsed_time =round(time.time() - startIndexTime,1)
+    elapsed_time = round(time.time() - start_index_time,1)
     print("Successfully indexed "+str(idx)+" movies")
     print(f"Elapsed time (indexing): {elapsed_time} seconds")
-
     writer.commit()
     writer.close()
     print("Successfully committed the movies")
 
 
 if __name__ == '__main__':
-
+    # Start of PyLucene VM
     lucene.initVM(vmargs=['-Djava.awt.headless=true'])
     print('lucene', lucene.VERSION)
-
-    #
+    # Read and index the movies
     data = pd.read_csv('../Movie_Plots.csv')
     indexMovies(StandardAnalyzer(), data)
--- PyLucene/SearchFiles.py
+++ PyLucene/SearchFiles.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
-
 INDEX_DIR = "IndexFiles.index"
 
 import os, lucene
-
 from java.nio.file import Paths
 from org.apache.lucene.analysis.standard import StandardAnalyzer
 from org.apache.lucene.index import DirectoryReader
@@ -11,12 +8,15 @@ from org.apache.lucene.queryparser.classic import QueryParser
 from org.apache.lucene.store import NIOFSDirectory
 from org.apache.lucene.search import IndexSearcher
 
+# Package for displaying texted wrapped in the window
 import textwrap
 
-def searchMovies(analyzer):
+
+def search_movies(analyzer):
+    # Configuration settings
     directory = NIOFSDirectory(Paths.get(os.path.join(os. getcwd(), INDEX_DIR)))
     searcher = IndexSearcher(DirectoryReader.open(directory))
-
+    # Loop for queries
     while True:
         print()
         print("Hit enter with no input to quit.")
@@ -27,25 +27,27 @@ def searchMovies(analyzer):
         print()
         print("Searching for:", command)
         query = QueryParser("plot", analyzer).parse(command)
-        scoreDocs = searcher.search(query, 50).scoreDocs
-        print("%s total matching documents." % len(scoreDocs))
-        id = 0;
+        score_docs = searcher.search(query, 50).scoreDocs
+        print("%s total matching documents." % len(score_docs))
 
-        for scoreDoc in scoreDocs:
-            id += 1
+        # id of each displayed plot
+        plot_id = 0
+        for scoreDoc in score_docs:
+            plot_id += 1
             doc = searcher.doc(scoreDoc.doc)
-            print(str(id), '\t', doc.get("year"),  '\t', doc.get("title"), '\t', '\t--> Score:', scoreDoc.score)
+            print(str(plot_id), '\t', doc.get("year"),  '\t', doc.get("title"), '\t', '\t--> Score:', scoreDoc.score)
 
-        if id>0:
-            while (True):
+        # If at least a plot was found then plot the title and prompt the user to see the plot
+        if plot_id > 0:
+            while True:
                 print()
                 print("Type the number of the movie you want to see the plot for.")
                 print("Type anything else to return to the query search.")
                 inp = input("Movie number: ")
                 if inp.isdigit():
                     parsed_number = int(inp)
-                    if 0 < parsed_number < len(scoreDocs):
-                        doc = searcher.doc(scoreDocs[parsed_number].doc)
+                    if 0 < parsed_number < len(score_docs):
+                        doc = searcher.doc(score_docs[parsed_number].doc)
                         plot = doc.get("plot")
                         if plot == "":
                             print("Sorry, this movie has no plot available")
@@ -56,8 +58,10 @@ def searchMovies(analyzer):
                     break
     del searcher
 
+
 if __name__ == '__main__':
+    # Start of PyLucene VM
     lucene.initVM(vmargs=['-Djava.awt.headless=true'])
     print('lucene', lucene.VERSION)
-
-    searchMovies(StandardAnalyzer())
+    # Start the query method
+    search_movies(StandardAnalyzer())
