738f8d2cc42d3a4aece702d03c567299c5cc86b4
Theofanis Tsiantas <th.tsiantas@icloud.com>
2024-06-25 08:15:48 +0200

    Addition of comments.

--- CIRS/processor.py
+++ CIRS/processor.py
@@ -8,14 +8,20 @@ STOPS_NLTK = stopwords.words('english')
 nlp = spacy.load('en_core_web_sm')
 
 
+# Method to eliminate all unnecessary words from the list of plots
 def eliminate_stopwords(plots: list):
     print("Starting removal of stopwords...")
+    # For every plot in the list of plots
     for plot_idx in range(0, len(plots)):
+        # Create a list of split words
         plot_words = plots[plot_idx].split()
+        # Define a list which will contain the necessary plot words
         cleaned_words = []
         for word in plot_words:
+            # Check in the standard NLTK-package
             if word.lower() not in STOPS_NLTK:
                 cleaned_words.append(word.lower())
+        # Replace the plot with the necessary words only
         plots[plot_idx] = ' '.join(cleaned_words)
 
         if plot_idx % 5000 == 0 and plot_idx > 0:
